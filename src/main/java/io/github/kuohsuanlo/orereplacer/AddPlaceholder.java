bf6d2bc2d2c90a257be206d3ea8bd26c9122225a
kuohsuanlo <kuohsuanlo@gmail.com>
2017-12-04 18:30:27 +0800

    add placeholder

---
+++ src/main/java/io/github/kuohsuanlo/orereplacer/AddPlaceholder.java
@@ -0,0 +1,30 @@
+package io.github.kuohsuanlo.orereplacer;
+
+import java.text.DecimalFormat;
+
+import javax.swing.text.NumberFormatter;
+
+import org.bukkit.entity.Player;
+
+import me.clip.placeholderapi.external.EZPlaceholderHook;
+
+public class AddPlaceholder extends EZPlaceholderHook{
+	private OreReplacerPlugin oplugin;
+
+	public AddPlaceholder(OreReplacerPlugin instance) {
+		super(instance, "coinhivemc");
+		// TODO Auto-generated constructor stub
+		oplugin = instance;
+	}
+
+	@Override
+	public String onPlaceholderRequest(Player p, String identifier) {
+		String player_name = p.getName().toLowerCase();
+		if (identifier.equals("increasing_constant")) {
+			return oplugin.PROBABILITY_INCREASING_CONSTANT+"";
+	    }
+		return "";
+	}
+
+
+}
